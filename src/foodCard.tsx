71e9e74e7b284f56abbe1b60df72c63ba882102b
sam-s04 <sam.sheedy4@gmail.com>
2025-03-13 12:31:10 -0400

    removing unnecessary parts and renaming some files

--- src/foodCard.tsx
+++ src/foodCard.tsx
@@ -10,11 +10,6 @@ const FoodCard = () => {
                 Example food description. This food looks so good.
             </p>
             </div>
-            <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
         </div>
     );
 }
