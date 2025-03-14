6bfad63d60bdea840a4e7876f4e0b2f71bea0bf3
aarav-nair <aarav.nair@gmail.com>
2025-03-14 11:46:16 -0400

    #13 Match FoodCard.tsx to Sketch

--- src/foodCard.tsx
+++ src/foodCard.tsx
@@ -11,16 +11,21 @@ type FoodCardProps = {
 }
 
 const FoodCard = ({ organization, food, location, description, price, distance, img }: FoodCardProps) => {
-    const foodImg = `/src/assets/${img}`;
+    const defaultImg = "/src/assets/placeholder.png";
+    const foodImg = img ? `/src/assets/${img}` : defaultImg;
     return (
         <div className="card">
             <img className="food-image" src={foodImg} alt="Food"></img>
-            <h2 className="org-name">{organization}</h2>
-            <h1 className="food-name">{food}</h1>
-            <h2 className="location">{location}</h2>
-            <p className="description">{description}</p>
-            <p className="price">{price}</p>
-            <p className="distance">{distance} miles</p>
+            <div className="food-details">
+                <h2 className="org-name">{organization}</h2>
+                <h1 className="food-name">{food}</h1>
+                <h2 className="location">{location}</h2>
+            </div>
+            <div className="food-details">
+                <p className="description">{description}</p>
+                <p className="price">{price}</p>
+                <p className="distance">{distance} miles</p>            
+            </div>
         </div>
     );
 }
