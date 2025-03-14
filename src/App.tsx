ed905261612595e7a8a5c7c6eba19924e4bcfb22
Manya Phutela <mphutela@umass.edu>
2025-03-14 18:15:34 -0400

    feat: Implement reusable banner
    - Added profile and logo images
    - Added app name and description
    - Adjusted UI styling for persistent banner
    
    Resolves #22

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
-import FoodCard from './FoodCard.tsx';
-import './App.css';
+import FoodCard from "./foodCard.tsx";
+import AppBanner from "./Banner.tsx";
+import "./App.css";
 
 function App() {
-
   const tempdata = {
-    organization: 'UMASO',
-    food: 'Cookies',
-    location: 'Hasbrouck Laboratory',
-    description: 'Example food description. This food looks so good.',
-    price: 'Free',
+    organization: "UMASO",
+    food: "Cookies",
+    location: "Hasbrouck Laboratory",
+    description: "Example food description. This food looks so good.",
+    price: "Free",
     distance: 0.2,
-    img: "pizza.jpg"
-  }
-  
+    img: "pizza.jpg",
+  };
+
   return (
-    <div>
-      <FoodCard
-        organization={tempdata.organization}
-        food={tempdata.food}
-        location={tempdata.location}
-        description={tempdata.description}
-        price={tempdata.price}
-        distance={tempdata.distance}
-        img={tempdata.img}
+    <div className="app-wrapper">
+      <AppBanner
+        logoSrc="/src/assets/logo.png"
+        name="Minuteman Meals"
+        desc="Find free food on campus!"
+        profileSrc="/src/assets/profile.png"
       />
-      <FoodCard organization={'Organization'} food={'Food'} location={'Location'} description={'Description'} price={'Price'} distance={0} img={"pizza.jpg"}/>
-      <FoodCard organization={'Organization'} food={'Food'} location={'Location'} description={'Description'} price={'Price'} distance={0} img={"pizza.jpg"}/>
+
+      <div className="app-container">
+        <FoodCard
+          organization={tempdata.organization}
+          food={tempdata.food}
+          location={tempdata.location}
+          description={tempdata.description}
+          price={tempdata.price}
+          distance={tempdata.distance}
+          img={tempdata.img}
+        />
+        <FoodCard
+          organization={"Organization"}
+          food={"Food"}
+          location={"Location"}
+          description={"Description"}
+          price={"Price"}
+          distance={0}
+          img={"pizza.jpg"}
+        />
+        <FoodCard
+          organization={"Organization"}
+          food={"Food"}
+          location={"Location"}
+          description={"Description"}
+          price={"Price"}
+          distance={0}
+          img={"pizza.jpg"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
