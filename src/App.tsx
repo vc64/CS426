e8aa7d32c0acad6123796d9c2418a3bb2f97b7a5
Manu Hegde <manu.hegde02@gmail.com>
2025-03-24 15:08:38 -0400

    Added comments for future implementation. This includes food card component, need to integrate this into a full homepage/dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,11 @@ function App() {
           <p className="text-gray-600 text-center mt-2">Save food and money by rescuing these meals</p>
         </header>
         
-        {/* Method to make this display as a grid - map it into an array of cards */}
+        {/* 
+        Method to make this display as a grid - map it into an array of cards.
+        When it comes to the homepage, have an array of all the food listings, and then sort the array based on the appropriate filters
+        This way, we can just map them to easily put them into a grid - we need to implement this into an actual homepage
+        */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {foodItems.map((item, index) => (
             <FoodCard key={index} food={item} />
