83ce23e933703cfa5176af32095180ce6de93979
Manu Hegde <manu.hegde02@gmail.com>
2025-03-24 16:34:47 -0400

    Add more mock data and have default sorting when page is loaded. Display active places first and prioritize those

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import FoodCard from './FoodCard';
-import { foodItems } from './data/foodItems';
+import { foodItemType, foodItems } from './data/foodItems';
 import './App.css';
 
 function App() {
@@ -7,6 +7,23 @@ function App() {
   // Mock data for the cards
   // Todo:
   // Sort by distance, time open, favorites, active 
+
+  // We want to separate first into two groups - active or not active
+  // Then order first by favorites, then by distance
+  const sortFood = (arr: foodItemType[]) => {
+    const favorites = arr.filter(e => e.isFavorite);
+    const notFavorites = arr.filter(e => !e.isFavorite);
+    favorites.sort((a, b) => a.distance - b.distance);
+    notFavorites.sort((a, b) => a.distance - b.distance);
+    return favorites.concat(notFavorites);
+  }
+
+  const active = foodItems.filter(e => e.active);
+  const notActive = foodItems.filter(e => !e.active);
+
+  const activeList = sortFood(active);
+  const notActiveList = sortFood(notActive);
+  const sortedFoodItems = activeList.concat(notActiveList)
 
   return (
     <div className="absolute inset-0 bg-white w-full min-h-screen">
@@ -22,7 +39,7 @@ function App() {
         This way, we can just map them to easily put them into a grid - we need to implement this into an actual homepage
         */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {foodItems.map((item, index) => (
+          {sortedFoodItems.map((item, index) => (
             <FoodCard key={index} food={item} />
           ))}
         </div>
