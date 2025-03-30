4150c08563ae66d57ae6c4f2891cc8ea08ff6536
vc64 <vichen2004@gmail.com>
2025-03-30 01:43:50 -0400

    add context for food listing form to allow components to know whether the form is open - Create add listing page #16

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { TagProvider } from "./contexts/TagContext";
+import { FoodListingProvider } from "./contexts/FoodListingContext";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TagProvider>
-      <App />
-    </TagProvider>
+    <FoodListingProvider>
+      <TagProvider>
+        <App />
+      </TagProvider>
+    </FoodListingProvider>
   </StrictMode>,
 )
